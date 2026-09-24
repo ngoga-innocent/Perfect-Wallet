9d54430f5fd1e91ad2b3eee40208a7c737b67ab9
Ngoga <Ngogainnocent1@gmail.com>
2026-09-24 17:01:11 +0200

    passenger_wsgi

---
+++ passenger_wsgi.py
@@ -0,0 +1,18 @@
+# passenger_wsgi.py
+
+import os
+import sys
+
+sys.path.insert(
+    0,
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "config.settings"
+)
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
